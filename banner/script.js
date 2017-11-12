16f6b4b87dbaed3c8cf7bd719aee3765077a2c21
mrjkc <julius.kvedaras@coupa.com>
2017-11-12 20:34:59 +0000

    banner

---
+++ banner/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var banner = document.querySelector("main");
+    var logo = document.querySelector("#logo");
+
+    setTimeout(function() {
+        banner.className = "visible";
+    }, 300);
+
+    banner.addEventListener("mouseover", function() {
+        banner.className = "over";
+        logo.className = "upsidedown";
+    });
+    banner.addEventListener("mouseout", function() {
+        banner.className = "visible";
+        logo.className = "";
+    });
+
+})
